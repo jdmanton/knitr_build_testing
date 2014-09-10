e76ce8883f005c75d3c137ed359c09b0e37d7f75
James Manton <ajd.manton@googlemail.com>
2014-09-10 14:23:17 +0100

    Fix lack of quotes...

--- buildall.sh
+++ buildall.sh
@@ -5,5 +5,5 @@ rmarkdown_files <- dir(pattern=".Rmd", recursive=TRUE)
 built_files <- sapply(rmarkdown_files, render)
 
 # built_files will not exist if a file failed to build
-if(!exists(built_files)) quit(status=1)
+if(!exists("built_files"")) quit(status=1)
 else quit(status=0)
